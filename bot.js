49229190a5c2cbe326fcfdf53eeef911065157ce
a-hawkins <36182040+a-hawkins@users.noreply.github.com>
2021-05-24 16:01:48 -0500

    Added overhead for dynamic command processing

--- bot.js
+++ bot.js
@@ -1,16 +1,44 @@
+const fs = require('fs');
+const { prefix } = require('./config.json');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
 const dotenv = require('dotenv');
 dotenv.config();
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(process.env.TOKEN);
+
 client.on('message', message => {
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) {
+		message.reply('Invalid Command.');
+		return;
+	}
+	try {
+		client.commands.get(command).execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('There was an error trying to execute that command!');
 	}
 });
+
+client.login(process.env.TOKEN);
